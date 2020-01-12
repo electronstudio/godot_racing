df5fe69fbe10b69cf4106afbed22970c3933cd4a
electronstudio <misc@fantastic.me.uk>
2020-01-12 02:54:58 +0000

    drift key

--- player.gd
+++ player.gd
@@ -3,7 +3,9 @@ extends RigidBody2D
 export var STEERING=100
 export var ACCELERATION=5
 export var FRICTION=0.1
-export var BREAKING=10
+export var DRIFT_FRICTION=1
+export var DRIFT_STEERING=100
+#export var BREAKING=10
 	
 func _physics_process(delta):
 	
@@ -18,20 +20,24 @@ func _physics_process(delta):
 #	else:
 #		linear_damp=FRICTION
 
-	#print("foo ", linear_velocity.length())
-	
 	var steering = Input.get_action_strength("steer_right") - Input.get_action_strength("steer_left")
-	apply_torque_impulse(STEERING * steering)
+	if Input.is_action_pressed("drift"):
+		apply_torque_impulse(DRIFT_STEERING * steering)
+		linear_damp = DRIFT_FRICTION
+	else:
+		var acceleration = Input.get_action_strength("accelerate") * Vector2.UP * ACCELERATION
+		apply_central_impulse(acceleration.rotated(rotation))
+		apply_torque_impulse(STEERING * steering ) #* clamp(linear_velocity.length()/300, 0, 1))
+		linear_damp = FRICTION
 
-	var acceleration = Input.get_action_strength("accelerate") * Vector2.UP * ACCELERATION
-	apply_central_impulse(acceleration.rotated(rotation))
-
-	var breaking = Input.get_action_strength("break")
-	linear_damp = lerp(FRICTION, BREAKING, breaking)
+	#ar breaking = Input.get_action_strength("break")
+	#linear_damp = lerp(FRICTION, BREAKING, breaking)
+	
+#	if acceleration.length_squared()<0.1 and not 
+#		linear_damp = BREAKING
+#	else:
+#		linear_damp = 0
 	
 	var scalefactor = 1 + linear_velocity.length()/1000
-	
 	$Camera2D.zoom = Vector2(scalefactor, scalefactor)
 #
-	#print(acceleration, breaking, linear_damp)
-	
